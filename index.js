e2c2d3d62c8af759a70b81be1a26c625cd8e18c1
Emmanuel Geoffray <emmanuel@soixantecircuits.fr>
2018-07-03 22:33:51 +0200

    Initial import

---
+++ index.js
@@ -0,0 +1,100 @@
+const SpacebroClient = require('spacebro-client').SpacebroClient
+var standardSettings = require('standard-settings')
+var MailListener = require('mail-listener-fixed')
+// const download = require('download')
+const express = require('express')
+const path = require('path')
+const mkdirp = require('mkdirp')
+const fs = require('fs-extra')
+const _ = require('lodash')
+var settings = standardSettings.getSettings()
+
+var spacebroClient = new SpacebroClient()
+_.set(settings.service.mail, 'attachmentOptions.directory', settings.folder.output + '/')
+settings.service.mail.debug = console.log
+var mailListener = new MailListener(settings.service.mail)
+mkdirp(settings.folder.output)
+mkdirp(settings.folder.tmp)
+
+mailListener.start() // start listening
+
+mailListener.on('connected', () => {
+  console.log('imapConnected')
+})
+
+mailListener.on('disconnected', () => {
+  console.log('imapDisconnected')
+  mailListener.start()
+})
+
+mailListener.on('error', (err) => {
+  console.log(err)
+})
+
+let getBucketAndToken = (address) => {
+  let bucketAndToken = {}
+  if (address === undefined) {
+    return bucketAndToken
+  }
+  let parts = address.split('@')
+  let localpart = parts[0]
+  parts = localpart.split('?')
+  bucketAndToken.bucket = parts[0]
+  if (parts && parts.length > 1) {
+    let params = parts[1].split('&')
+    for (let param of params) {
+      parts = param.split('=')
+      let key = parts[0]
+      let value
+      if (parts && parts.length > 1) {
+        value = parts[1]
+      }
+      bucketAndToken[key] = value
+    }
+  }
+  return bucketAndToken
+}
+console.log(getBucketAndToken('britney-pompadour?token=BR1TN3Y@socialite.paris'))
+
+let mailListenerMediaToStandardMedia = (mail) => {
+  let mailObject = JSON.parse(JSON.stringify(mail))
+  delete mailObject.attachments
+  delete mailObject.eml
+  let media = {
+    meta: {
+      email: mail.to.value,
+      melkibro: mailObject
+    }
+  }
+
+  if (mail.attachments) {
+    let file = mail.attachments[0]
+    fs.moveSync(file.path, path.join(settings.folder.output, file.filename), { overwrite: true })
+    media.url = `http://${settings.server.host}:${settings.server.port}/${file.filename}`
+  }
+
+  let bucketAndToken = getBucketAndToken(mail.from.value[0].address)
+  if (bucketAndToken.bucket) {
+    media.meta.altruist = {
+      socialite: {
+        bucket: bucketAndToken.bucket,
+        token: bucketAndToken.token
+      },
+      mandrill: {
+        template: bucketAndToken.bucket
+      }
+    }
+  }
+
+  return media
+}
+
+mailListener.on('mail', (mail, seqno, attributes) => {
+  let outMedia = mailListenerMediaToStandardMedia(mail)
+  // spacebroClient.emit(settings.service.spacebro.client.out.outMedia.eventName, outMedia)
+  console.log('emit ' + JSON.stringify(outMedia, null, 2))
+})
+
+var app = express()
+app.use(express.static(settings.folder.output))
+app.listen(process.env.PORT || settings.server.port)
---
+++ readme.md
@@ -0,0 +1,47 @@
+# melkibro
+
+A simple app to listen emails on an inbox and send them as media on spacebro.  
+
+## ❓Why ?
+
+It is a helpful input to send media in the spacebro galaxy. Send an
+email to the inbox and then process it by making connections between
+spacebro apps.
+
+For example, you can send a picture to the mailbox, connect it to
+blurrybro to blur it, connect it to altruistify/print to print it.
+
+## 🌍 Installation
+
+```
+git clone https://github.com/soixantecircuits/melkibro
+nvm use
+yarn
+```
+
+
+## ⚙ Configuration
+
+```
+cp settings/settings.default.json settings/settings.json
+```
+
+And edit settings.json
+You can also change settings with argv parameters.
+Learn more about this on [standard-settings](https://github.com/soixantecircuits/standard-settings)
+
+
+## 👋 Usage
+
+```
+nvm use
+npm start
+```
+
+## 🕳 Troubleshooting
+
+Ask emmanuel on soixantecircuits.slack.com
+
+## ❤️ Contribute
+
+Please do!
